a1b3f1e56da5d10d2585a535db8ba64aaf59ed78
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2022-07-06 14:01:04 +0530

    updated

---
+++ ContactBook.Web/ClientApp/src/components/ContactList.jsx
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+
+
+class ContactList extends React.Component {
+
+    render() {
+        return(
+        <>
+            {
+                this.props.contacts.map((contact) => (
+                    <div class="boxes" id="box1" onClick={() => this.props.onContactSelected(contact.id) }>
+                        <p style={{ paddingBottom: "5px", fontSize: "28px" }}>{contact.name}</p>
+                        <p style={{ marginLeft: "5px" }}>{contact.email}</p>
+                        <p style={{ paddingBottom: "5px", marginLeft: "5px" }} >+91 {contact.mobile}</p>
+                    </div>
+                ))
+            }
+        </>
+        )
+    }
+}
+
+export default ContactList;
--- ContactBook.Web/ClientApp/src/components/Home.js
+++ ContactBook.Web/ClientApp/src/components/Home.js
@@ -3,6 +3,7 @@ import  './main.css';
 //import './main.jsx';
 import blog from '../Images/blog-icon.png';
 import Contact from './Contact';
+import ContactList from './ContactList';
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -32,44 +33,79 @@ export class Home extends Component {
     }
 
     
-    contacts = [
-    {
-        id: 1,
-        name: "Chandermani Arora",
-        email: "chandermani@technovert.com",
-        mobile: "904 543 6009",
-        landline: "786 21 3612456",
-        website: "gooogle.com",
-        address: "52 A/2 Mathura"
-    },
-    {
-        id: 2,
-        name: "Vaibhav Taunk",
-        email: "vaibhav.T@technovert.com",
-        mobile: "904 587 6765",
-        landline: "876 34 8364298",
-        website: "yahoo.com",
-        address: "123 Hyderawad"
-    },
-    {
-        id: 3,
-        name: "Bhagavan",
-        email: "Bhagawan.T@technovert.com",
-        mobile: "876 543 6009",
-        landline: "879 63 2785437",
-        website: "microsoft.com",
-        address: "876 Mathura "
-    },
-    {
-        id: 4,
-        name: "Pujitha",
-        email: "pujitha.k@technovert.com",
-        mobile: "835 443 6009",
-        landline: "323 42 7354238",
-        website: "technovert.com",
-        address: "345 Mathura"
+
+    constructor() {
+        super();
+        this.state = {
+            contacts: [
+                {
+                    id: 1,
+                    name: "Chandermani Arora",
+                    email: "chandermani@technovert.com",
+                    mobile: "904 543 6009",
+                    landline: "786 21 3612456",
+                    website: "gooogle.com",
+                    address: "52 A/2 Mathura"
+                },
+                {
+                    id: 2,
+                    name: "Vaibhav Taunk",
+                    email: "vaibhav.T@technovert.com",
+                    mobile: "904 587 6765",
+                    landline: "876 34 8364298",
+                    website: "yahoo.com",
+                    address: "123 Hyderawad"
+                },
+                {
+                    id: 3,
+                    name: "Bhagavan",
+                    email: "Bhagawan.T@technovert.com",
+                    mobile: "876 543 6009",
+                    landline: "879 63 2785437",
+                    website: "microsoft.com",
+                    address: "876 Mathura "
+                },
+                {
+                    id: 4,
+                    name: "Pujitha",
+                    email: "pujitha.k@technovert.com",
+                    mobile: "835 443 6009",
+                    landline: "323 42 7354238",
+                    website: "technovert.com",
+                    address: "345 Mathura"
+                }
+            ],
+
+            currContact: null
+        }
+
+        this.setCurrContact = this.setCurrContact.bind(this);
     }
-    ]
+
+    componentDidMount() {
+        this.setState({ currContact: this.state.contacts[0] });
+    }
+
+    setCurrContact(id) {
+        this.setState({ currContact: this.state.contacts.find(c => c.id === id) });
+    }
+
+    
+    // renderContacts() {
+    //let htmlString;
+    //let contactList = document.querySelector("#contactList");
+    //     {
+    //         contacts.forEach(contact => {
+    //         htmlString += `<div class="boxes" id="box1" onclick="showContactInfo(${contact.id})">
+    //    <p style="padding-bottom: 5px; font-size:28px;">${contact.name}</p>
+    //    <p style="margin-left:5px">${contact.email}</p>
+    //    <p style="padding-bottom: 5px; margin-left:5px">+91 ${contact.mobile}</p>
+    //  </div>`;
+    //         })
+    //     };
+    //contactList.innerHTML = htmlString;
+    //}
+
 
   render () {
     return (
@@ -91,51 +127,15 @@ export class Home extends Component {
                     <p style={{margin: "30px 0 20px 20px", fontSize: "18px", fontWeight: "bold"}}>
                             CONTACTS
                         </p>
-                        <div class="box" id="contactList">
+                    <div class="box" id="contactList" >
+                        <ContactList contacts={this.state.contacts} onContactSelected={this.setCurrContact} />
                         </div>
                     </div>
                 <div
                     id="viewBox"
                     class="middleRight"
-                    style={{marginLeft: "150px", marginTop: "60px", display: "inline-Block"}}>
-                    <Contact contact={
-                        {
-                            id: 1,
-                            name: "Chandermani Arora",
-                            email: "chandermani@technovert.com",
-                            mobile: "904 543 6009",
-                            landline: "786 21 3612456",
-                            website: "gooogle.com",
-                            address: "52 A/2 Mathura"
-                        },
-                        {
-                            id: 2,
-                            name: "Vaibhav Taunk",
-                            email: "vaibhav.T@technovert.com",
-                            mobile: "904 587 6765",
-                            landline: "876 34 8364298",
-                            website: "yahoo.com",
-                            address: "123 Hyderawad"
-                        },
-                        {
-                            id: 3,
-                            name: "Bhagavan",
-                            email: "Bhagawan.T@technovert.com",
-                            mobile: "876 543 6009",
-                            landline: "879 63 2785437",
-                            website: "microsoft.com",
-                            address: "876 Mathura "
-                        },
-                        {
-                            id: 4,
-                            name: "Pujitha",
-                            email: "pujitha.k@technovert.com",
-                            mobile: "835 443 6009",
-                            landline: "323 42 7354238",
-                            website: "technovert.com",
-                            address: "345 Mathura"
-                        }
-} />
+                    style={{ marginLeft: "150px", marginTop: "60px", display: "inline-Block" }}>
+                    {this.state.currContact ? (<Contact contact={this.state.currContact} />) : (<></>)}
                 </div>
                 </div>
             
